56647d6ab1875d3aa2b056509557a22086ef53f9
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-15 13:13:16 +0000

    テスト以外完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
       @book = Book.new(book_params)
@@ -17,7 +18,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
      redirect_to book_path(@book.id)
     else
-     render action: :new
+     @books = Book.all
+     render :index
     end
   end
   #   @book = Book.new(book_params)
@@ -29,15 +31,26 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
+    # if @book.save
+    #   flash[:notice] = "Book was successfully created."
+    # redirect_to book_path(@book.id)
+    # else
+    # @books = Book.all
+    # render :index
+    # end
+
+    # @books = Book.all
   end
 
-  def dupdate
+  def update
       @book = Book.find(params[:id])
-    if book.update
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-     redirect_to books_path
+     redirect_to book_path(@book.id)
     else
-     render action: :new
+      @books = Book.all
+     render :edit
     end
   end
 
@@ -62,6 +75,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
