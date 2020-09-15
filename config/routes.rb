1a4b1e092657580a299b74aa3964d9dc7f060114
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-15 14:11:22 +0000

    [Finish]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,13 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  
-  
-  Rails.application.routes.draw do
+
+
+Rails.application.routes.draw do
   root 'home#top'
 end
-  
-  
-  
-#   > app.root_path
-# => "/"
 
-# > app.root_url
-# => "http://www.example.com/"
 
-# > app.url_for(controller: 'home', action: 'index')
-# => "http://www.example.com/"
+
 
 end
