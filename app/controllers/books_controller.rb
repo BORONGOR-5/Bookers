7265e9a65b3e9a71e0686eb86b69c0927c82d4e2
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-15 08:50:56 +0000

    途中６-dstroy実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,6 +22,21 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    
+  end
+  
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  
+  
+  
 
   private
   def book_params
